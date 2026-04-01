69d64b169f81a06437907b35bd9dd01c33f10eed
iammanikandanp <manisunny632@gmail.com>
2026-04-01 17:13:45 +0530

    bug fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  // credentials: true,
 }));
 
 app.get("/health", (req, res) => {
